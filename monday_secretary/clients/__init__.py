fc259be0ce6cb1f81f496358288d685a686d7cde
yawatako <yawata.three.personalities@gmail.com>
2025-06-17 16:32:22 +0900

    Update __init__.py

--- monday_secretary/clients/__init__.py
+++ monday_secretary/clients/__init__.py
@@ -4,10 +4,12 @@ from .health   import HealthClient
 from .calendar import CalendarClient
 from .memory   import MemoryClient
 from .work     import WorkClient
+from .acceptance import AcceptanceClient
 
 __all__ = [
     "HealthClient",
     "CalendarClient",
     "MemoryClient",
     "WorkClient",
+    "AcceptanceClient",
 ]
